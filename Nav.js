dd8929fcec0d9984ac1d6447b949084e9956e52d
Jordan Parfitt <jordanparfitt@gmail.com>
2021-01-27 16:32:27 -0700

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -11,18 +11,18 @@ export class Nav {
     this.activeTarget.className = "highlighted-image";
   }
 
-  up() {
+  up(masterData) {
     var rect = this.activeTarget.getBoundingClientRect();
     //TODO: Loop through sibling and find closest image
     var newTargetImage = document.elementFromPoint(rect.x + 10, rect.y - 75);
     if (newTargetImage && newTargetImage.tagName === "IMG") {
       this.setActiveTarget(newTargetImage);
       this.highlightAndScroll("vert");
-      this.setNavLabel();
+      this.setNavLabel(masterData);
     }
   }
 
-  down() {
+  down(masterData) {
     var rect = this.activeTarget.getBoundingClientRect();
     //TODO: Loop through sibling and find closest image
     var newTargetImage = document.elementFromPoint(
@@ -32,28 +32,28 @@ export class Nav {
     if (newTargetImage && newTargetImage.tagName === "IMG") {
       this.setActiveTarget(newTargetImage);
       this.highlightAndScroll("vert");
-      this.setNavLabel();
+      this.setNavLabel(masterData);
     }
   }
 
-  left() {
+  left(masterData) {
     // left arrow
     if (this.activeTarget.previousSibling) {
       this.setActiveTarget(this.activeTarget.previousSibling);
       this.highlightAndScroll("horz");
-      this.setNavLabel();
+      this.setNavLabel(masterData);
     }
   }
 
-  right() {
+  right(masterData) {
     if (this.activeTarget.nextElementSibling) {
       this.setActiveTarget(this.activeTarget.nextElementSibling);
       this.highlightAndScroll("horz");
-      this.setNavLabel();
+      this.setNavLabel(masterData);
     }
   }
 
-  setNavLabel() {
+  setNavLabel(masterData) {
     var idArray = this.activeTarget.id.split("-");
     var activeItemJson = masterData.rows[idArray[1]].items[idArray[2]];
     var currentSelectionLabel = document.getElementById(
@@ -73,7 +73,7 @@ export class Nav {
     }
   }
 
-  enter() {
+  enter(masterData) {
     var idArray = this.activeTarget.id.split("-");
     document.getElementById("modalImage").src = masterData.getBestImage(
       idArray[1],
